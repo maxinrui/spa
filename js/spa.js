7c3752dda089286524d38e112d35c2855e734838
Xinrui Ma <xinrui@simple-task.com>
2014-11-05 23:16:10 -0500

    init spa project

---
+++ js/spa.js
@@ -0,0 +1,34 @@
+/*
+ * spa.js
+ * Root namespace module
+ */
+
+/*jslint
+    devel  : true,
+    newcap : true,
+    regexp : true,
+    white  : true,
+    browser : true,
+    indent : 2,
+    nomen : true,
+    sloppy : true,
+    continue : true,
+    maxerr  : 50,
+    plusplus : true,
+    vars : false
+*/
+
+/*global $ */
+
+var spa = (function () {
+    var initModule = function ($container) {
+        $container.html(
+            
+        );
+    };
+
+    return {
+        initModule: initModule
+    };
+
+}());
